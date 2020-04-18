9dddc49544de05b781015e3a823a83b377c0e68e
Teguh Faddila <57081125+fhuget@users.noreply.github.com>
2020-04-19 00:24:17 +0700

    Update FakultasSeeder.php

--- database/seeds/FakultasSeeder.php
+++ database/seeds/FakultasSeeder.php
@@ -16,7 +16,7 @@ class FakultasSeeder extends Seeder
         $listFakultas = [
             'Fakultas Ilmu Komputer', 
             'Fakultas Hukum',
-            'Fakultas Ilmu Budaya',
+            'Fakultas Ilmu udaya',
             'Fakultas Teknik',
             'Fakultas Kedokteran',
             'Fakultas Matematika dan Ilmu Pengetahuan Alam',
