73c1f6d144c59f72168141497dcad45f6dadc76b
Teguh Faddila <57081125+fhuget@users.noreply.github.com>
2020-05-07 08:06:01 +0700

    Weekly Assignments 05 - Import Excel

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -32,7 +32,11 @@
             <a href="{{ route('fakultas.create') }}">
               <button type="button" class="btn btn-primary">Add New</button>
             </a>
+
+             <button type="button" class="btn btn-success" data-toggle="modal" data-target="#importExcel">Import Excel</button>
+
           </div>
+
           <div class="card-body">
             <table class="table table-bordered">
               <thead>
@@ -80,3 +84,32 @@
 </section>
 {!! $data->links() !!}
 @endsection()
+
+          <!-- Import Excel -->
+
+              <div class="modal fade" id="importExcel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <form method="post" action="fakultas/import" enctype="multipart/form-data">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
+                      </div>
+                      <div class="modal-body">
+           
+                        {{ csrf_field() }}
+           
+                        <label>Pilih file excel</label>
+                        <div class="form-group">
+                          <input type="file" name="file" required="required">
+                        </div>
+           
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Import</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <!-- End Import Excel -->
